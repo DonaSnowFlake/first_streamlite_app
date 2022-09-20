b7957a66e0d1b4abcbeb7181cb49464549758989
Dona <114000295+DonaSnowFlake@users.noreply.github.com>
2022-09-20 17:22:16 +0300

    Update streamlit_app.py
    Add header and menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 
 import streamlit
 streamlit.title ('My Parents New Healty Dinner')
+streamlit.header ('Breakfast Menu')
+streamlit.text ('Omlet')
+streamlit.text ('Bluberry Oatmeal')
+streamlit.text ('Fruits')
+
+
+
