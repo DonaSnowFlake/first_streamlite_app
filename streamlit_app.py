eec3c31f9bd82ed85d12850d73153c095f94c6e5
Dona <114000295+DonaSnowFlake@users.noreply.github.com>
2022-10-13 11:20:40 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,4 +58,4 @@ streamlit.dataframe(my_data_rows)
 fruit_search = streamlit.text_input('What fruit would you like ito add?','bannana')
 
 #Allow the end user to add a fruit to a list 
-#my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlite');")
+#my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit');")
